62454216b7d0426c23d75ba4aafd761093447e63
Jay MOULIN <jaymoulin@gmail.com>
2017-10-29 22:56:25 +0100

    Add js_externs parameter for API

--- compiler.py
+++ compiler.py
@@ -7,6 +7,7 @@ def main():
     parser.add_argument("--js", default='/dev/stdin', help="Input file")
     parser.add_argument("--js_output_file", default='/dev/stdout', help="Output file")
     parser.add_argument("--compilation_level", default='WHITESPACE_ONLY', choices=['WHITESPACE_ONLY', 'SIMPLE_OPTIMIZATIONS', 'ADVANCED_OPTIMIZATIONS'], help="Compilation level")
+    parser.add_argument("--js_externs", default='', help="Declare some external js vars and functions separated with semicolon")
     args = parser.parse_args()
     js_code = open(args.js, 'r')
 
@@ -15,6 +16,7 @@ def main():
         ('compilation_level', args.compilation_level),
         ('output_format', 'text'),
         ('output_info', 'compiled_code'),
+        ('js_externs', args.js_externs),
     ])
 
     js_code.close()
